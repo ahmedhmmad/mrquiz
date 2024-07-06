e1fc39a25690de08942b69e7528b60655771524c
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-07-06 03:59:17 +0300

    fixes

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -54,7 +54,7 @@ export class InfrastructureStack extends cdk.Stack {
 
     // Deploy React app to S3 bucket
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-      sources: [s3deploy.Source.asset('./web/build')],
+      sources: [s3deploy.Source.asset('../web/build')],
       destinationBucket: bucket,
     });
   }
